5b56dc5f66349e16d75b43ca3669a575940aa7c1
jacobvanlingen <je.lingen@portofrotterdam.com>
2023-08-25 18:28:31 +0200

    Improve readability

--- src/main/java/org/tenggara/GetterProcessor.java
+++ src/main/java/org/tenggara/GetterProcessor.java
@@ -1,12 +1,9 @@
 package org.tenggara;
 
 import com.sun.source.tree.Tree;
-import com.sun.tools.javac.code.Flags;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeTranslator;
 import com.sun.tools.javac.util.List;
-import com.sun.tools.javac.util.ListBuffer;
-import com.sun.tools.javac.util.Name;
 import org.tenggara.annotations.Getter;
 
 import javax.annotation.processing.ProcessingEnvironment;
@@ -14,10 +11,11 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import java.util.Set;
+
+import static com.sun.tools.javac.code.Flags.PUBLIC;
 
 @SupportedAnnotationTypes("org.tenggara.annotations.Getter")
 @SupportedSourceVersion(SourceVersion.RELEASE_17)
@@ -31,42 +29,42 @@ public class GetterProcessor extends BaseProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        Set<? extends Element> set = roundEnv.getElementsAnnotatedWith(Getter.class);
-        set.forEach(element -> {
-            JCTree jcTree = trees.getTree(element);
-            jcTree.accept(new TreeTranslator() {
+        final var getterElements = roundEnv.getElementsAnnotatedWith(Getter.class);
+        getterElements.forEach(element -> {
+            trees.getTree(element).accept(new TreeTranslator() {
                 @Override
                 public void visitClassDef(JCTree.JCClassDecl jcClassDecl) {
-                    List<JCTree.JCVariableDecl> jcVariableDeclList = List.nil();
-                    for (JCTree tree : jcClassDecl.defs) {
-                        if (tree.getKind().equals(Tree.Kind.VARIABLE)) {
-                            JCTree.JCVariableDecl jcVariableDecl = (JCTree.JCVariableDecl) tree;
-                            jcVariableDeclList = jcVariableDeclList.append(jcVariableDecl);
-                        }
-                    }
-                    jcVariableDeclList.forEach(jcVariableDecl -> {
-                        messager.printMessage(Diagnostic.Kind.NOTE, jcVariableDecl.getName() + " has been processed");
-                        jcClassDecl.defs = jcClassDecl.defs.prepend(makeGetterMethodDecl(jcVariableDecl));
+                    final var classVariableDeclarations = jcClassDecl.defs.stream()
+                            .filter(it -> it.getKind() == Tree.Kind.VARIABLE)
+                            .map(JCTree.JCVariableDecl.class::cast)
+                            .map(it -> makeGetterMethodDecl(it))
+                            .toList();
+
+                    classVariableDeclarations.forEach(it -> {
+                        messager.printMessage(Diagnostic.Kind.NOTE, it.getName() + " has been processed");
+                        jcClassDecl.defs = jcClassDecl.defs.prepend(it);
                     });
+
                     super.visitClassDef(jcClassDecl);
                 }
-
             });
         });
         return true;
     }
 
-    private JCTree.JCMethodDecl makeGetterMethodDecl(JCTree.JCVariableDecl jcVariableDecl) {
-        ListBuffer<JCTree.JCStatement> statements = new ListBuffer<>();
-        statements.append(treeMaker.Return(treeMaker.Select(treeMaker.Ident(names.fromString("this")), jcVariableDecl.getName())));
-        JCTree.JCBlock body = treeMaker.Block(0, statements.toList());
+    private JCTree.JCMethodDecl makeGetterMethodDecl(JCTree.JCVariableDecl classVariableDeclaration) {
+        final var name = classVariableDeclaration.getName();
+        final var field = treeMaker.Select(treeMaker.Ident(names.fromString("this")), name);
+        final var returnStatement = treeMaker.Return(field);
 
-        return treeMaker.MethodDef(treeMaker.Modifiers(Flags.PUBLIC), getNewMethodName(jcVariableDecl.getName()), jcVariableDecl.vartype, List.nil(), List.nil(), List.nil(), body, null);
+        final var modifiers = treeMaker.Modifiers(PUBLIC);
+        final var returnType = classVariableDeclaration.vartype;
+        final var methodName = names.fromString("get" + name.toString().substring(0, 1).toUpperCase() + name.toString().substring(1, name.length()));
+        final var parameterTypes = List.<JCTree.JCTypeParameter>nil();
+        final var parameterObjects = List.<JCTree.JCVariableDecl>nil();
+        final var exceptions = List.<JCTree.JCExpression>nil();
+        final var codeBlock = treeMaker.Block(0, List.of(returnStatement));
+
+        return treeMaker.MethodDef(modifiers, methodName, returnType, parameterTypes, parameterObjects, exceptions, codeBlock, null);
     }
-
-    private Name getNewMethodName(Name name) {
-        String s = name.toString();
-        return names.fromString("get" + s.substring(0, 1).toUpperCase() + s.substring(1, name.length()));
-    }
-
 }
